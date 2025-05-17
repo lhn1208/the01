65374fb836e76bd0d6c86709c3da900638008c5e
lhn <lhn1208@naver.com>
2025-05-17 16:17:56 +0900

    업로드2

--- ch04_word/src/component/CreateDay.jsx
+++ ch04_word/src/component/CreateDay.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 const CreateDay = () => {
+   const days = [1,2,3];
+   const navigate = useNavigate();
+   const addDay = () =>{
+      fetch(`http://localhost:3010/days`,{
+         method:"POST",
+         headers: {
+            "Content-Type" : "application/json"
+         },
+         body:JSON.stringify({
+            id:"" + (days.length+1),
+            day: days.length+1
+         })
+      }) //post방식
+      .then(res =>{
+         if(res.ok){
+            alert("생성 완료");
+            navigate('/');
+         }else{
+            alert("생성 실패");
+            navigate('/');
+         }
+      })
+   }
   return (
-    <div>CreateDay</div>
+    <div>
+      <h3>현재 일수 : {days.length}</h3>
+      <button onClick={addDay}>Day 추가</button>
+    </div>
   )
 }
 
